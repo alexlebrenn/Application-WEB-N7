207e8ed89af93582f537ba2dcb17ded14dd0e931
Alex Le Brenn <alexandre.lebrenn@etu-enseeiht.fr>
2011-11-04 09:22:53 +0100

    Initialisation

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6bdd3e7d841cb886b06aa09a8102ed80ee08406757d6047951f82dc85b8b89f829956aa5def7240ee592813b341ad577ea807730529033d227bec36421b6c777'
